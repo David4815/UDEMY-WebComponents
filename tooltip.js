a2045bef77f76f1846626a4a6f003f344489c7a6
DavidHomeWin <sdavid4815@gmail.com>
2025-08-27 22:17:16 -0300

    add tooltip

--- tooltip.js
+++ tooltip.js
@@ -1,15 +1,29 @@
 class Tooltip extends HTMLElement {
-    constructor() {
-        super();
-        
-    }
+  constructor() {
+    super();
+    this._tooltipContainer;
+  }
 
-    connectedCallback(){
-const tooltipIcon = document.createElement('span');
-        tooltipIcon.textContent = ' (?)'; // Info icon
-        this.appendChild(tooltipIcon);
-        // this.attachShadow({ mode: 'open' });
-    }
+  connectedCallback() {
+    const tooltipIcon = document.createElement("span");
+    tooltipIcon.textContent = " (?)"; // Info icon
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
+    this.appendChild(tooltipIcon);
+    // this.attachShadow({ mode: 'open' });
+  }
+
+
+  _showTooltip() {
+    this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.textContent = 'This is a tooltip text';
+    this.appendChild(this._tooltipContainer);
+  }
+
+  _hideTooltip() {
+    this.removeChild(this._tooltipContainer);
+  }
+
 }
 
-customElements.define('my-tooltip', Tooltip);
+customElements.define("my-tooltip", Tooltip);
